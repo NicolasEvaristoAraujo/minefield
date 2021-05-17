3e52d7b149d6cbd622ebfe791dff6dcea2c8ebfb
Nicolas <araujoevaristo@gmail.com>
2021-05-16 23:21:53 -0300

    Added the logical for identify the next blocks of select block.

--- src/br/com/nea/mf/model/Camp.java
+++ src/br/com/nea/mf/model/Camp.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Camp {
-	
+		
 	private final int line;
 	private final int colum;
 	
@@ -13,10 +13,30 @@ public class Camp {
 	private boolean flagged = false;
 	
 	private List<Camp> neighbors = new ArrayList<>();
-	
+
+	//Construtor
 	Camp(int line, int colum){
 		this.line = line;
 		this.colum = colum;
 	}
-	
+
+	boolean addNeighbor(Camp neighbor){
+		boolean lineDiff = line != neighbor.line;
+		boolean columDiff = colum != neighbor.colum;
+		boolean diagonal = lineDiff && columDiff;
+
+		int deltLine = Math.abs(line - neighbor.line);
+		int deltColum = Math.abs(colum - neighbor.colum);
+		int deltGlobal = deltColum + deltLine;
+
+		if (deltGlobal == 1 && !diagonal){
+			neighbors.add(neighbor);
+			return true;
+		}else if (deltGlobal == 2 && diagonal){
+			neighbors.add(neighbor);
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
